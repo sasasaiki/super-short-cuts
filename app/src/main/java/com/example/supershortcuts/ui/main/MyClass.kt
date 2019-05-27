e3abbe0b73c59123c8da3d109b6c73dcb1a7a3e6
saiki <otoiawasehakotiradayo@gmail.com>
2019-05-27 16:58:33 +0900

    alt+cmd+m

---
+++ app/src/main/java/com/example/supershortcuts/ui/main/MyClass.kt
@@ -0,0 +1,34 @@
+package com.example.supershortcuts.ui.main
+
+class MyClass {
+
+    fun outPutName(first: String, last: String, upperCase: Boolean = false) {
+        val fullName = if (upperCase) {
+            first + last
+        } else {
+            (first + last).toUpperCase()
+        }
+
+        println(fullName)
+    }
+
+    fun outPutNameAndScore(first: String, last: String, upperCase: Boolean = false, score: Int) {
+        val fullName = if (upperCase) {
+            first + last
+        } else {
+            (first + last).toUpperCase()
+        }
+
+        println("$fullName score:$score")
+    }
+
+    fun outPutNameAndAge(first: String, last: String, age: Int, upperCase: Boolean = false) {
+        val fullName = if (upperCase) {
+            first + last
+        } else {
+            (first + last).toUpperCase()
+        }
+
+        println("$fullName age:$age")
+    }
+}
